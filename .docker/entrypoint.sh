199c30d67552ca4b258e9e07ce3a714b9f256b2a
Pedro Rodrigues <pir.pedro@gmail.com>
2021-02-25 22:52:05 -0300

    cp .env file during bootstrap

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,6 +3,7 @@
 npm config set cache /home/node/app/.npm-cache --global
 
 cd /home/node/app
+if [ ! -f ".env" ]; then cp .env.example .env; fi
 npm install
-while sleep 1000; do :; done
-# nodemon -L #legacy (importante quando está usando docker)
+# while sleep 1000; do :; done
+nodemon -L #legacy (importante quando está usando docker)
